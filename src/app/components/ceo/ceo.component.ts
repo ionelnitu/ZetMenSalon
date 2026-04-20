408040e071d090a40403154661fdcf97d8482fda
unknown <ionelnitu73@gmail.com>
2026-04-20 19:53:18 +0300

    fixing images

--- src/app/components/ceo/ceo.component.ts
+++ src/app/components/ceo/ceo.component.ts
@@ -27,13 +27,13 @@ export class CeoComponent implements AfterViewInit {
 
   accomplishments: Accomplishment[] = [
     {
-      image: '/assets/firstCertificate.jpeg',
+      image: 'assets/firstCertificate.jpeg',
       year: '2025',
       title: 'Fade Academy by Ciprian Ungureanu',
       desc: " Este locul unde am invatat tehnici avansate de fade si formele de top. Aici am aflat ca frizerii sunt ca o familie si ca promotorul acestui curent este Ciprian Ungureanu.  Dupa acest curs am plecat cu un bagaj consistent de informatii, motivat si pregatit sa fac fata oricarei provocari. In afara de tehnici aici am avut parte de mentoring si subiecte motivationale. Un loc unde m-am simtit acasa. Iar asta iti da incredere si siguranta ca esti acolo unde trebuie. Am plecat cu o sete imensa de a ma descoperi si a ma provoca pentru a deveni varianta mea mai buna, zi de zi.",
     },
     {
-      image: '/assets/2ndCertificate.jpg',
+      image: 'assets/2ndCertificate.jpg',
       year: '2024',
       title: 'The Quarters Academy Birmingham,',
       desc: 'Aici am avut curs intensiv de barbering cu o echipa extraordinara. Am studiat partea teoretica si practica la un nivel academic in ceea ce priveste tehnicile de fade si foarfeca, tipurile de par, anatomia scalpului si alegerea formei tunsorii. Un curs care te ajuta sa intelegi daca mergi mai departe sau nu.  Unde doar pasiunea si dorinta de a fi mai bun ca ieri, sunt criteriile de baza.',
